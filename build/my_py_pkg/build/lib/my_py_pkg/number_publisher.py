2de556564dfb3d71aed7a5bfa914aaf436532f01
gaetano <guysblues@gmail.com>
2023-09-12 15:04:59 -0700

    Created a server and client_no_oop

--- build/my_py_pkg/build/lib/my_py_pkg/number_publisher.py
+++ build/my_py_pkg/build/lib/my_py_pkg/number_publisher.py
@@ -4,13 +4,13 @@ from rclpy.node import Node
 from example_interfaces.msg import Int64
 
 
-class NumberPublisher(Node):
+class NumberPublisherNode(Node):
     def __init__(self):
         super().__init__("number_publisher")
 
         self.number = 2
         self.publisher = self.create_publisher(Int64, "number", 10)
-        self.timer = self.create_timer(1, self.publish_news)
+        self.timer = self.create_timer(1, self.publish_number)
         self.get_logger().info("Number Publisher has been started")
 
     def publish_number(self):
@@ -21,7 +21,7 @@ class NumberPublisher(Node):
 
 def main(args=None):
     rclpy.init(args=args)
-    node = NumberPublisher()
+    node = NumberPublisherNode()
     rclpy.spin(node)
     rclpy.shutdown()
 
