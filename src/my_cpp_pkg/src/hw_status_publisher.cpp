3f4e66d4926126fa651d8b1d344772c44ba4c8a5
gaetano <guysblues@gmail.com>
2023-09-18 10:51:36 -0700

    Fixed Custom msg Publisher CPP

--- src/my_cpp_pkg/src/hw_status_publisher.cpp
+++ src/my_cpp_pkg/src/hw_status_publisher.cpp
@@ -1,15 +1,15 @@
 #include "rclcpp/rclcpp.hpp"
 #include "my_robot_interfaces/msg/hardware_status.hpp"
 
-class HardwareStatusPublisherNode : public rclcpp::Node
+class HardwareStatusPublisher : public rclcpp::Node
 {
 public:
-    HardwareStatusPublisherNode() : Node("hardware_status")
+    HardwareStatusPublisher() : Node("hardware_status_publisher")
     {
         pub_ = this->create_publisher<my_robot_interfaces::msg::HardwareStatus>(
             "hardware_status", 10);
         timer_ = this->create_wall_timer(
-            std::chrono::seconds(q), std::bind(&HardwareStatusPublisherNode::timer_callback, this));
+            std::chrono::seconds(1), std::bind(&HardwareStatusPublisher::publishHardwareStatus, this));
         RCLCPP_INFO(this->get_logger(), "Hardware Status Publisher has been started.");
     }
 
@@ -30,7 +30,7 @@ private:
 int main(int argc, char **argv)
 {
     rclcpp::init(argc, argv);
-    auto node = std::make_shared<HardwareStatusPublisherNode>();
+    auto node = std::make_shared<HardwareStatusPublisher>();
     rclcpp::spin(node);
     rclcpp::shutdown();
     return 0;
